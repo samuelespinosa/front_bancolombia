5d6d1d980bc1a63c88bdfdf5d7a3fc3946b6e344
Samuel Espinosa <saespinosab@unal.edu.co>
2024-05-12 17:31:23 -0500

    final

--- src/sections/search/header.jsx
+++ src/sections/search/header.jsx
@@ -1,11 +1,9 @@
 import '../../App.css';
-import useNavigate from 'react-router-dom';
 export default function Header() {
-  const navigate=useNavigate(); 
   return (
     <div style={{textAlign:'center',}}>
-      <figure onClick={()=>navigate('/')} className="logo">
-        <img src={logo} alt="logo"/>
+      <figure className="logo">
+        <img src="logo.svg"/>
       </figure>
     </div>
   );
